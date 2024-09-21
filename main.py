f94784cc08860d01d93bfa536fa58447e62649ab
Pranjal Udhwani <51302832+Pranjal0410@users.noreply.github.com>
2024-09-21 23:36:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def replace_word():
 
-    str="hi guys, I am Priya, and I love to draw"
+    str="hi guys, I am Priya, and I love to draw" 
     word_to_replace= input("Enter the word to replace: ")
     word_replacement = input("Enter the word replacement: ")
     print(str.replace(word_to_replace, word_replacement))
